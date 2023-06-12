39adc234d638430846c868638b901f5da7321ad0
Averines <averines@gmail.com>
2023-06-13 01:19:33 +0300

    add: product card

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,13 @@ window.addEventListener('click', (e) => {
     if (e.target.classList.contains("accordion__title")) {
         e.target.closest(".accordion__item").classList.toggle("is-opened");
     }
+
+    if (e.target.classList.contains("product-card__fav")) {
+        if (!e.target.classList.contains("is-active")) {
+            e.target.classList.toggle("is-active");
+            e.target.innerHTML = "<span></span>";
+        } else {
+            e.target.classList.remove("is-active");
+        }
+    }
 })
